e4d1d008461515d00dc41ddcc1851165ef7f2611
Douglas Pinheiro <dougcpinheiro@yahoo.com>
2018-03-20 20:15:52 -0300

    Adding Readme.md

--- README.md
+++ README.md
@@ -1,11 +1,67 @@
 # SkipTheDishes Java Challenge
 
-# JAVA
 # Build an API to:
-	Allow Authentication
-	Query Products
-	Receive Orders
-	Cancel an Order
-	Get Order Status
-	Store data in a database of his/her choice
+	
+		
+*   Allow Authentication
+*   Query Products
+*	Receive Orders
+*	Cancel an Order
+*	Get Order Status
+*	Store data in a database of his/her choice
+	
+# Database configuration
+
+**	Database: MySQL **
+*	spring.datasource.url = jdbc:mysql://localhost:3306/skip?useSSL=false
+*	spring.datasource.username = skip
+*	spring.datasource.password = skip
+*	spring.datasource.testWhileIdle = true
+*	spring.datasource.validationQuery = SELECT 1
+*	spring.jpa.show-sql = true
+*	spring.jpa.hibernate.ddl-auto = create-drop
+*	spring.jpa.hibernate.naming-strategy = org.hibernate.cfg.ImprovedNamingStrategy
+*	spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.MySQL5Dialect
+
+	
+# JUnit Tests
+
+**Authentication Test:**
+	
+		@Test
+		public void authenticationTest() {
+			ResponseEntity<Authentication> auth = customerController.auth("test@test.com", "12345");
+			assertEquals("Authentication failed", auth.getStatusCode(), HttpStatus.OK);
+		}
+	
+**Quering Products Test:**
+	
+		@Test
+		public void queryProducts() {
+			ResponseEntity<Product> productQuery = productController.findProductById(1);
+			Store s = new Store(6, "Store 6");
+			Product product_3 = new Product(1, "Big Mac", "Big Mac Combo", 5.00, s);
+			assertEquals("Quering product 3 failed!", new ResponseEntity<Product>(product_3, HttpStatus.OK).getStatusCode(), 		productQuery.getStatusCode());
+		}
+
+**Receiving Order and saving to database:**
+	
+		@Test
+		public void receiveOrderAndSaveToDatabase() throws NoSuchAlgorithmException {
+			Customer c = customerController.findCustomerById(1).getBody();
+			Store s = new Store(1, "Store 100");
+			List<OrderItem> items = new ArrayList<OrderItem>(0);
+			items.add(new OrderItem(1, productController.findProductById(3).getBody(), 5.00, 10.00, 2));
+			Order o = new Order("2018-03-19T23:33:28.580Z", c, "Address 2", "Contact Teste 2", s, 25.00, "NEW", "2018-03-19T23:33:28.580Z",items);
+			ResponseEntity<Order> productQuery = orderController.save(o);
+			assertEquals("Order reciver failed!", new ResponseEntity<Order>(o, HttpStatus.OK).getStatusCode(), productQuery.getStatusCode());
+		}
+	
+**Canceling Order and getting order status:**
+	
+		@Test
+		public void cancelOrderAndGetOrderStatus() {
+			Order o = orderController.cancelOrder(1).getBody();
+			assertEquals("Canceling order failed!", o.getStatus(), "CANCELED");
+		}
 
--- src/main/java/dougpinheiro/skip/challenge/ChallengeApplication.java
+++ src/main/java/dougpinheiro/skip/challenge/ChallengeApplication.java
@@ -1,110 +1,14 @@
 package dougpinheiro.skip.challenge;
 
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import dougpinheiro.skip.challenge.controller.CustomerController;
 
 @SpringBootApplication
 public class ChallengeApplication {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
-		ApplicationContext ctx = SpringApplication.run(ChallengeApplication.class, args);
-		System.out.println(new String(Base64.getEncoder().encode(MessageDigest.getInstance("MD5").digest(new String("12345").getBytes()))));
-		
-		
-		
-		CustomerController ac = (CustomerController) ctx.getBean("customerController");
-		
-		
-		System.out.println("#################################################################");
-		System.out.println("Testing the login method...");
-		System.out.println(ac.auth("test@test.com", "12345"));
-		System.out.println("#################################################################\n\n\n");
-		
-		/*
-		System.out.println("#################################################################");
-		System.out.println("Inserting products...");
-		
-		ProductController pc = (ProductController) ctx.getBean("productController");
-		
-		Stream<Integer> ids = Stream.iterate(1, n  ->  n  + 1).limit(15);
-		
-		DecimalFormat df = new DecimalFormat("$#.##");
-		
-		ids.forEach((n)->{
-			pc.insertProduct(new Product("Product Example No "+n, Math.random()*30));
-		});
-		
-		System.out.println("Listing products inserted...");
-		
-		pc.getAllProducts().stream().forEach((p)->{
-			System.out.println(p.toString());
-		});
-		
-		System.out.println("Products inserted!");
-		System.out.println("#################################################################\n\n\n");
-		
-		
-		System.out.println("#################################################################");
-		System.out.println("Creating an order...");
-		
-		ClientOrder myOrder = new ClientOrder();
-		Set<ClientOrders> registration = new HashSet<ClientOrders>();
-
-		ClientOrderController clientOrderController = (ClientOrderController) ctx.getBean("clientOrderController");
-		clientOrderController.createOrder(myOrder);
-		
-		//get a sort of products
-		//register all the products to the order
-		
-		Stream<Integer> qtdProducts = Stream.iterate(1, (x) -> x+1).limit(Math.round(Math.random()*10));
-		
-		qtdProducts.forEach((c)->{
-			if(Math.round(Math.random()*pc.getAllProducts().size()) > 0) {
-				ClientOrders co = new ClientOrders(pc.findById(Math.round(Math.random()*pc.getAllProducts().size())), myOrder);
-				ClientOrdersController coc = (ClientOrdersController) ctx.getBean("clientOrdersController");
-				coc.save(co);
-				registration.add(co);
-			}
-		});
-		
-		myOrder.setClientOrders(registration);
-		
-		System.out.println("Order created!");
-		System.out.println("#################################################################\n\n\n");
-		System.out.println("#################################################################");
-		
-		System.out.println("Showing order...");
-		
-		System.out.println("ORDER : "+myOrder.getIdClientOrder());
-		System.out.println("ORDER_TIME : "+myOrder.getOrderTime().toString());
-		System.out.println("STATUS : "+myOrder.getStatus());
-		System.out.println("PRODUCTS: ------------------------------------------------------");
-		
-		//list all products from the order
-		myOrder.getClientOrders().stream().forEach((c) -> {
-			System.out.println(c.getProduct().getDescription());
-		});
-		
-		System.out.println("#################################################################\n\n\n");
-		System.out.println("#################################################################");
-		
-		System.out.println("Canceling the order...");
-		clientOrderController.cancelOrder(myOrder);
-		System.out.println("Order Canceled");
-		System.out.println("#################################################################\n\n\n");
-		
-		System.out.println("#################################################################");
-		System.out.println("Checking order status...");
-		System.out.println("Order "+myOrder.getIdClientOrder()+" - "+myOrder.getStatus());
-		System.out.println("#################################################################");*/
-		
-		
+		SpringApplication.run(ChallengeApplication.class, args);
 	}
 }
